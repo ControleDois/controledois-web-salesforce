c35c28b25959e3040d23614d7a2ddbde9b54532a
Thiago Ribeiro <trl.debrito@controledois.com.br>
2024-11-12 18:23:01 -0400

    🐛 Internet
    Ajuste de verificação de internet

--- src/app/shared/services/internet.service.ts
+++ src/app/shared/services/internet.service.ts
@@ -9,7 +9,8 @@ export class InternetService {
       const start = performance.now();
       await fetch('https://google.com.br', { method: 'HEAD', cache: 'no-cache', mode: 'no-cors' });
       const latency = performance.now() - start;
-      return latency > 200; // Considera boa conexão se a latência for menor que 200ms
+      console.log(latency);
+      return latency <= 6000; // Considera boa conexão se a latência for menor que 200ms
     } catch {
       return false; // Sem internet ou conexão ruim
     }
